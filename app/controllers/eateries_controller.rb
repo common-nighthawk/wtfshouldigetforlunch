29c68b0337eb6576eaef45d4232660793c3f5d32
Daniel <danielallendeutsch@gmail.com>
2015-06-08 20:13:48 -0400

    allows cusines types to be selected and unselected through params.

--- app/controllers/eateries_controller.rb
+++ app/controllers/eateries_controller.rb
@@ -2,6 +2,10 @@ class EateriesController < ApplicationController
   before_action :find_company
 
   def index
+    params[:cuisines].reject! { |c| c.empty? } if params[:cuisines]
     @eateries = @company.eateries
+    @cuisines = @company.cuisines
+    @selected_cuisines = params[:cuisines] ? params[:cuisines].map { |x| @cuisines.find_by(name:x) } : Cuisine.where(name: 'ajfj')
+    @non_selected_cuisines = @cuisines - @selected_cuisines
   end
 end
